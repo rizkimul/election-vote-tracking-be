37d9cef970197749baac2e79beb68a0b3f537363
rizkimul <riizkimaul4@gmail.com>
2025-11-12 22:06:22 +0700

    feat(router): add try and exception in votes router

--- app/routers/votes.py
+++ app/routers/votes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi.responses import JSONResponse
 from ..schemas import VoteCreate, VoteOut
 from ..deps import get_vote_service, get_current_user
 from typing import List, Optional
@@ -7,8 +8,25 @@ router = APIRouter(prefix="/votes", tags=["votes"])
 
 @router.post("/", response_model=VoteOut)
 def create_vote(payload: VoteCreate, svc = Depends(get_vote_service), user = Depends(get_current_user)):
-    # user is available if you want to record who created the vote
-    return svc.add_vote(payload)
+    try:
+        # user is available if you want to record who created the vote
+        return svc.add_vote(payload)
+    except HTTPException as e:
+        return JSONResponse(
+            status_code=e.status_code,
+            content={
+                "status": e.status_code,
+                "message": e.detail
+            }
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status": 500,
+                "message": str(e) if e else "Internal Server Error"
+            }
+        )
 
 @router.get("/", response_model=List[VoteOut])
 def list_votes(page: int = Query(1, ge=1),
@@ -19,6 +37,23 @@ def list_votes(page: int = Query(1, ge=1),
                election_year: Optional[int] = None,
                svc = Depends(get_vote_service),
                user = Depends(get_current_user)):
-    res = svc.list_votes(page=page, size=size, district=district, sub_district=sub_district, village=village, election_year=election_year)
-    # return only items list to match response_model=List[VoteOut] OR return full meta — here we'll return items only
-    return res["items"]
+    try:
+        res = svc.list_votes(page=page, size=size, district=district, sub_district=sub_district, village=village, election_year=election_year)
+        # return only items list to match response_model=List[VoteOut] OR return full meta — here we'll return items only
+        return res["items"]
+    except HTTPException as e:
+        return JSONResponse(
+            status_code=e.status_code,
+            content={
+                "status": e.status_code,
+                "message": e.detail
+            }
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status": 500,
+                "message": str(e) if e else "Internal Server Error"
+            }
+        )
