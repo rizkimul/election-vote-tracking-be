6ac150b0c6341aabb5f3555ce6216a96003afd5a
rizkimul <riizkimaul4@gmail.com>
2025-11-12 22:07:30 +0700

    feat(main): add cors middleware and change app name

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from .database import engine
 from . import models
 from .routers import auth, votes, engagements, uploads
@@ -6,7 +8,26 @@ from .routers import auth, votes, engagements, uploads
 # Create tables if you want (Alembic will handle migrations in production)
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Election Vote Tracking API - Service/Repository (Sync)")
+app = FastAPI(title="Election Vote Tracking API")
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.middleware("http")
+async def catch_exceptions(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={"error": str(e)}
+        )
 
 app.include_router(auth.router)
 app.include_router(votes.router)
