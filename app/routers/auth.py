b6e10d2c877bc514e6d7fd97be1d8c658c41f42e
rizkimul <riizkimaul4@gmail.com>
2025-11-12 22:06:48 +0700

    feat(router): add try and exception in auth router

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from ..schemas import UserCreate, UserOut, LoginSchema
 from ..deps import get_auth_service
 
@@ -9,13 +10,45 @@ def register(user: UserCreate, auth_svc=Depends(get_auth_service)):
     try:
         new = auth_svc.register(user.nik, user.name, user.password)
         return new
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    except HTTPException as e:
+        return JSONResponse(
+            status_code=e.status_code,
+            content={
+                "status": e.status_code,
+                "message": e.detail,
+            }
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status": 500,
+                "message": str(e) if e else "Internal server error",
+            }
+        )
 
 @router.post("/login")
 def login(payload: LoginSchema, auth_svc=Depends(get_auth_service)):
-    user = auth_svc.authenticate(payload.nik, payload.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-    token = auth_svc.create_token(user.nik)
-    return token
+    try:
+            
+        user = auth_svc.authenticate(payload.nik, payload.password)
+        if not user:
+            raise HTTPException(status_code=400, detail="Invalid credentials")
+        token = auth_svc.create_token(user.nik)
+        return token
+    except HTTPException as e:
+        return JSONResponse(
+            status_code=e.status_code,
+            content={
+                "status": e.status_code,
+                "message": e.detail,
+            }
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status": 500,
+                "message": str(e) if e else "Internal server error",
+            }
+        )
